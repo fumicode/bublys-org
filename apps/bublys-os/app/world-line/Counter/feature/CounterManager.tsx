3d981b29514e646cf913c0be719c92bb50a6cdb4
kanetaku1 <ublvsp010111@gmail.com>
2025-10-19 21:55:34 +0900

    Counterオブジェクトの移動

---
+++ apps/bublys-os/app/world-line/Counter/feature/CounterManager.tsx
@@ -0,0 +1,22 @@
+import { Counter } from '../domain/Counter';
+
+/**
+ * Counterのシリアライズ
+ */
+export function serializeCounter(counter: Counter): any {
+  return counter.toJson();
+}
+
+/**
+ * Counterのデシリアライズ
+ */
+export function deserializeCounter(data: any): Counter {
+  return Counter.fromJson(data);
+}
+
+/**
+ * Counterの初期状態を作成
+ */
+export function createInitialCounter(initialValue: number = 0): Counter {
+  return new Counter(initialValue);
+}
